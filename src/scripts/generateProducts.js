d738aad45ae991750b9388ac2e7e39571b6697f4
a-nester <a_nester@ukr.net>
2024-07-20 11:03:11 +0300

    task-1

---
+++ src/scripts/generateProducts.js
@@ -0,0 +1,14 @@
+import { readFile } from "fs";
+import { createFakeProduct } from "../utils/createFakeProduct.js";
+import fs from "fs/promises";
+import { PATH_DB } from "../constants/path.js";
+
+const generateProducts = async (number) => {
+  const newProducts = Array.from({ length: number }, createFakeProduct);
+
+  const existData = await fs.readFile(PATH_DB, "utf-8");
+  const newData = [...JSON.parse(existData), ...newProducts];
+  await fs.writeFile(PATH_DB, JSON.stringify(newData, null, 2), "utf-8");
+};
+
+generateProducts(3);
