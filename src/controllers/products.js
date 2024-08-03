fa65a8bfd4019615a76d2a6ecfe341bd7b011dce
ok-qa <olechka.kr@gmail.com>
2024-08-03 02:56:14 -0700

    created controller and rout for deleting product

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,11 +1,24 @@
-import { getProducts } from "../services/products.js";
+import createHttpError from 'http-errors';
+import { deleteProduct, getProducts } from '../services/products.js';
 
 export const getAllProduct = async (req, res) => {
-    const products = await getProducts();
+  const products = await getProducts();
 
-    res.status(200).json({
-        status: 200,
-        message: "Successfully found products!",
-        data: products,
-    });
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found products!',
+    data: products,
+  });
+};
+
+export const deleteProductController = async (req, res, next) => {
+  const { productId } = req.params;
+  //   console.log("",productId);
+  const product = await deleteProduct(productId);
+  //   console.log(product);
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+  // res.status(204).end();
+  res.sendStatus(204);
 };
